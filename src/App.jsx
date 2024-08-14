8a616f603d821179af2c84db489547f91fe341e1
Badrú <sbsbadroo@gmail.com>
2024-08-14 13:25:35 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { RouterProvider } from "react-router-dom";
+import { route } from "./routes";
+
+const App = () => {
+return(
+  <>
+  <RouterProvider router={route} />
+ </>
+)
+
+};
+
+export default App;
+
+
+
+
