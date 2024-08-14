389e15f9f61067965293a36618ded1e76736cb72
Badrú <sbsbadroo@gmail.com>
2024-08-14 18:55:33 +0200

    fiat:Header

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,11 @@
 import { Card } from "../components/Card/card"
+import { Header } from "../components/global/header"
 
 export const Home = () => {
     return (
+
         <div data-theme="light">
+            <Header/>
 
             <h1 className=" text-black">Ola Mundo</h1>
             <button className="btn btn-active btn-primary">Primary</button>
