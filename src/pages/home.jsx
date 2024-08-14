4444baa395c9d66a74eea4b0dde0f78f5c269161
InacioMugana <inaciomugaua@gmail.com>
2024-08-14 18:45:01 +0200

    update:card

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,10 @@
-import { Card } from "../components/Card/card"
+import { Card } from "../components/Card/card";
 
 export const Home = () => {
-    return (
-        <div data-theme="light">
-
-            <h1 className=" text-black">Ola Mundo</h1>
-            <button className="btn btn-active btn-primary">Primary</button>
-            <Card/>
-        </div>
-        
-    )
-}
+  return (
+    <div data-theme="light">
+      <button className="btn btn-active btn-primary">Primary</button>
+      <Card />
+    </div>
+  );
+};
