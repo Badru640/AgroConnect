ee6dc5e542290f540fe5e57cb8b37105e7f944b6
Badrú <sbsbadroo@gmail.com>
2024-08-14 16:09:21 +0200

    main: update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
+import { Card } from "../components/Card/card"
+
 export const Home = () => {
     return (
         <div data-theme="light">
 
             <h1 className=" text-black">Ola Mundo</h1>
             <button className="btn btn-active btn-primary">Primary</button>
+            <Card/>
         </div>
         
     )
