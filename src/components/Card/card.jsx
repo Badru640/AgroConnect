bd93025deaccd28a408cb4d416de3f31c8461573
InacioMugana <inaciomugaua@gmail.com>
2024-08-15 07:32:18 +0200

    update handlePurche

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,170 +1,222 @@
 import { useEffect, useState } from "react";
 import { FaTimes } from "react-icons/fa";
 import { FiAlertCircle } from "react-icons/fi";
-import { useCart } from "../CartContext"; // Import your cart context
-import { useAuth } from "../../AuthContext"; // Import useAuth
+import { useCart } from "../CartContext";
+import { useAuth } from "../../AuthContext";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"; // Importar framer-motion
+import { motion } from "framer-motion";
 
 const ProductModal = ({ product, onClose }) => {
-    if (!product) return null;
+  if (!product) return null;
 
-    // Criar a mensagem para o WhatsApp
-    const createWhatsAppMessage = () => {
-        return encodeURIComponent(`Olá! Estou interessado no produto ${product.name}.\n\nDescrição: ${product.description}\nPreço: ${product.price} mt\n\nGostaria de mais informações.`);
-    };
-
-    // Criar o link para o WhatsApp
-    const whatsappLink = `https://wa.me/847640433?text=${createWhatsAppMessage()}`; // Substitua "847640433" pelo número desejado
-
-    return (
-        <div className="fixed inset-0 bg-green-900 bg-opacity-70 flex items-center justify-center z-50">
-            <div className="bg-white p-8 rounded-lg shadow-lg w-11/12 max-w-4xl relative">
-                <button
-                    className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 text-2xl"
-                    onClick={onClose}
-                    aria-label="Close modal"
-                >
-                    <FaTimes />
-                </button>
-                <h2 className="text-2xl font-bold mb-4 text-green-800">{product.name}</h2>
-                <img
-                    src={product.imagem || "/default-image.jpg"}
-                    alt={product.name}
-                    className="w-full h-80 object-cover mb-4 rounded-lg"
-                    onError={(e) => (e.target.src = "/default-image.jpg")}
-                />
-                <p className="text-gray-800 mb-4 text-lg">{product.description}</p>
-                <p className="text-green-700 font-bold text-xl mb-4">{product.price} mt</p>
-                <a
-                    href={whatsappLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
-                >
-                    Enviar Mensagem no WhatsApp
-                </a>
-            </div>
-        </div>
+  const createWhatsAppMessage = () => {
+    return encodeURIComponent(
+      `Olá! Estou interessado no produto ${product.name}.\n\nDescrição: ${product.description}\nPreço: ${product.price} mt\n\nGostaria de mais informações.`
     );
+  };
+
+  const whatsappLink = `https://wa.me/847640433?text=${createWhatsAppMessage()}`;
+
+  return (
+    <div className="fixed inset-0 bg-green-900 bg-opacity-70 flex items-center justify-center z-50">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-11/12 max-w-4xl relative">
+        <button
+          className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 text-2xl"
+          onClick={onClose}
+          aria-label="Close modal"
+        >
+          <FaTimes />
+        </button>
+        <h2 className="text-2xl font-bold mb-4 text-green-800">
+          {product.name}
+        </h2>
+        <img
+          src={product.imagem || "/default-image.jpg"}
+          alt={product.name}
+          className="w-full h-80 object-cover mb-4 rounded-lg"
+          onError={(e) => (e.target.src = "/default-image.jpg")}
+        />
+        <p className="text-gray-800 mb-4 text-lg">{product.description}</p>
+        <p className="text-green-700 font-bold text-xl mb-4">
+          {product.price} mt
+        </p>
+        <a
+          href={whatsappLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
+        >
+          Enviar Mensagem no WhatsApp
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export const Card = () => {
-    const [products, setProducts] = useState([]);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const { addToCart } = useCart();
-    const { isAdmin } = useAuth(); // Get user info and role
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [notification, setNotification] = useState(null); // Added state for notification
+  const { addToCart } = useCart();
+  const { isAdmin } = useAuth();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await fetch("http://localhost:3036/api/products");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch products");
-                }
-                const data = await response.json();
-                setProducts(data);
-            } catch (error) {
-                console.error("Error fetching products:", error);
-            }
-        };
-
-        fetchProducts();
-    }, []);
-
-    const handleDetailsClick = (product) => {
-        setSelectedProduct(product);
-    };
-
-    const handleCloseModal = () => {
-        setSelectedProduct(null);
-    };
-
-    const handleAddToCart = (product) => {
-        addToCart(product);
-    };
-
-    const handlePurchaseComplete = async (product) => {
-        try {
-            // Call to backend to mark the purchase as complete
-            const response = await fetch(`http://localhost:3036/api/products/${product._id}/complete`, {
-                method: 'PUT',
-            });
-            if (!response.ok) {
-                throw new Error('Failed to update product status');
-            }
-            // Remove the product from local state
-            setProducts(prevProducts => prevProducts.filter(p => p._id !== product._id));
-        } catch (error) {
-            console.error('Error completing purchase:', error);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3036/api/products");
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
         }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
     };
 
-    return (
-        <div className="flex flex-col min-h-screen bg-green-50">
-            <main className="flex-grow mt-6 p-4">
-                {isAdmin && (
-                    <Link to="/Add-Anuncio">
-                        <button className="bg-green-500 text-white py-2 px-6 rounded-lg hover:bg-green-600 transition-colors duration-300 mb-6">
-                            Criar Anúncio
-                        </button>
-                    </Link>
-                )}
-                <div className="products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {products.map((product) => (
-                        <motion.div
-                        
-                            key={product._id}
-                            className="border border-green-200 rounded-lg shadow-lg p-4 bg-white bg-opacity-80 transition-transform transform hover:scale-105"
-                            style={{
-                                backgroundImage: "url('/textures/wood-bg.jpg')",
-                                backgroundSize: "cover",
-                            }}
-                            initial={{ opacity: 0, y: 20 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{ duration: 0.3 }}
-                        >
-                            <img
-                                src={product.imagem || "/default-image.jpg"}
-                                alt={product.name}
-                                className="w-full h-40 object-cover mb-4 rounded-lg border-2 border-green-300"
-                                onError={(e) => (e.target.src = "/default-image.jpg")}
-                            />
-                            <h3 className="text-xl font-semibold mb-2 text-green-700">{product.name}</h3>
-                            <p className="text-gray-700 text-sm mb-2">{product.description}</p>
-                            <p className="text-green-600 font-semibold text-lg mb-2">{product.price} mt</p>
-                            <div className="flex gap-3 justify-between mt-2">
-                                <button
-                                    className="bg-green-700 text-white px-4 py-2 rounded-lg hover:bg-green-800 transition"
-                                    onClick={() => handleDetailsClick(product)}
-                                >
-                                    <FiAlertCircle />
-                                </button>
-                                {!isAdmin && (
-                                    <button
-                                        className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
-                                        onClick={() => handleAddToCart(product)}
-                                    >
-                                        Adicionar ao cesto
-                                    </button>
-                                )}
-                                {isAdmin && (
-                                    <button
-                                        className="bg-green-700 text-white px-4 py-2 rounded-lg hover:bg-green-800 transition"
-                                        onClick={() => handlePurchaseComplete(product)}
-                                    >
-                                        Compra Concluída
-                                    </button>
-                                )}
-                            </div>
-                        </motion.div>
-                    ))}
-                </div>
+    fetchProducts();
+  }, []);
 
-                {selectedProduct && (
-                    <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+  const handleDetailsClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProduct(null);
+  };
+
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
+  const handlePurchaseComplete = async (product) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3036/api/products/${product._id}/complete`,
+        {
+          method: "PUT",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to update product status");
+      }
+
+      // Update state to remove the product from the list
+      setProducts((prevProducts) =>
+        prevProducts.filter((p) => p._id !== product._id)
+      );
+
+      // Set success notification
+      setNotification({
+        type: "success",
+        message: "Compra concluída com sucesso!",
+      });
+
+      // Clear the notification after a short delay
+      setTimeout(() => {
+        setNotification(null);
+      }, 5000);
+    } catch (error) {
+      console.error("Error completing purchase:", error);
+      setNotification({
+        type: "error",
+        message: "Erro ao concluir a compra. Tente novamente mais tarde.",
+      });
+
+      // Clear the notification after a short delay
+      setTimeout(() => {
+        setNotification(null);
+      }, 5000);
+    }
+  };
+
+  // Function to close notification
+  const handleNotificationClose = () => {
+    setNotification(null);
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-green-50">
+      <main className="flex-grow mt-6 p-4">
+        {isAdmin && (
+          <Link to="/Add-Anuncio">
+            <button className="bg-green-500 text-white py-2 px-6 rounded-lg hover:bg-green-600 transition-colors duration-300 mb-6">
+              Criar Anúncio
+            </button>
+          </Link>
+        )}
+        <div className="products grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {products.map((product) => (
+            <motion.div
+              key={product._id}
+              className="border border-green-200 rounded-lg shadow-lg p-4 bg-white bg-opacity-80 transition-transform transform hover:scale-105"
+              style={{
+                backgroundImage: "url('/textures/wood-bg.jpg')",
+                backgroundSize: "cover",
+              }}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <img
+                src={product.imagem || "/default-image.jpg"}
+                alt={product.name}
+                className="w-full h-40 object-cover mb-4 rounded-lg border-2 border-green-300"
+                onError={(e) => (e.target.src = "/default-image.jpg")}
+              />
+              <h3 className="text-xl font-semibold mb-2 text-green-700">
+                {product.name}
+              </h3>
+              <p className="text-gray-700 text-sm mb-2">
+                {product.description}
+              </p>
+              <p className="text-green-600 font-semibold text-lg mb-2">
+                {product.price} mt
+              </p>
+              <div className="flex gap-3 justify-between mt-2">
+                <button
+                  className="bg-green-700 text-white px-4 py-2 rounded-lg hover:bg-green-800 transition"
+                  onClick={() => handleDetailsClick(product)}
+                >
+                  <FiAlertCircle />
+                </button>
+                {!isAdmin && (
+                  <button
+                    className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition"
+                    onClick={() => handleAddToCart(product)}
+                  >
+                    Adicionar ao cesto
+                  </button>
                 )}
-            </main>
+                {isAdmin && (
+                  <button
+                    className="bg-green-700 text-white px-4 py-2 rounded-lg hover:bg-green-800 transition"
+                    onClick={() => handlePurchaseComplete(product)}
+                  >
+                    Compra Concluída
+                  </button>
+                )}
+              </div>
+            </motion.div>
+          ))}
         </div>
-    );
+
+        {selectedProduct && (
+          <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+        )}
+
+        {/* Notification display */}
+        {notification && (
+          <div
+            className={`fixed bottom-4 right-4 p-4 rounded-lg text-white cursor-pointer ${
+              notification.type === "success" ? "bg-green-500" : "bg-red-500"
+            }`}
+            onClick={handleNotificationClose}
+          >
+            {notification.message}
+          </div>
+        )}
+      </main>
+    </div>
+  );
 };
--- src/components/addAnuncio.jsx
+++ src/components/addAnuncio.jsx
@@ -5,10 +5,10 @@ import { Header } from "./global/header";
 
 export const AddAnuncio = () => {
   const [formData, setFormData] = useState({
-    name: "zfzvzv",
-    description: "cxzxzas",
+    name: "",
+    description: "",
     price: 5566,
-    category: "66b505b328d71067d891b3c8",
+    category: "",
   });
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
@@ -44,100 +44,99 @@ export const AddAnuncio = () => {
 
   return (
     <div>
-<Header/>
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center relative">
-      <Link to="/" className="inline-block absolute top-2 left-2">
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          Home Page
-        </button>
-      </Link>
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-3xl">
-        <h2 className="text-2xl font-bold mb-6 text-left">Add Produts</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-            <div>
+      <Header />
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center relative">
+        <Link to="/" className="inline-block absolute top-2 left-2">
+          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+            Home Page
+          </button>
+        </Link>
+        <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-3xl">
+          <h2 className="text-2xl font-bold mb-6 text-left">Add Produts</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 text-left font-medium"
+                >
+                  Produts Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Name of the Produts"
+                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="categoryId"
+                  className="block text-gray-700 text-left font-medium"
+                >
+                  Categoria ID
+                </label>
+                <input
+                  type="text"
+                  id="categoryId"
+                  name="categoryId"
+                  value={formData.category}
+                  onChange={handleChange}
+                  placeholder="categoria ID"
+                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
+                  required
+                />
+                <label
+                  htmlFor="address"
+                  className="block text-gray-700 text-left font-medium mt-4"
+                >
+                  Localizacao
+                </label>
+                <input
+                  type="text"
+                  id="price"
+                  name="price"
+                  placeholder="Price of the produts"
+                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+            </div>
+            <div className="mb-6">
               <label
-                htmlFor="name"
+                htmlFor="description"
                 className="block text-gray-700 text-left font-medium"
               >
-                Produts Name
+                Store Description
               </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
+              <textarea
+                id="description"
+                name="description"
+                value={formData.description}
                 onChange={handleChange}
-                placeholder="Name of the Produts"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
-                required
-              />
+                placeholder="Produts Description"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500 h-32 resize-none"
+              ></textarea>
             </div>
-            <div>
-              <label
-                htmlFor="categoryId"
-                className="block text-gray-700 text-left font-medium"
+
+            {error && <p className="text-red-500 mb-4">{error}</p>}
+            {success && <p className="text-green-500 mb-4">{success}</p>}
+
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
               >
-                Categoria ID
-              </label>
-              <input
-                type="text"
-                id="categoryId"
-                name="categoryId"
-                value={formData.category}
-                onChange={handleChange}
-                placeholder="categoria ID"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
-                required
-              />
-              <label
-                htmlFor="address"
-                className="block text-gray-700 text-left font-medium mt-4"
-              >
-                Localizacao
-              </label>
-              <input
-                type="text"
-                id="price"
-                name="price"
-                placeholder="Price of the produts"
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500"
-              />
+                Register
+              </button>
             </div>
-          </div>
-          <div className="mb-6">
-            <label
-              htmlFor="description"
-              className="block text-gray-700 text-left font-medium"
-            >
-              Store Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleChange}
-              placeholder="Produts Description"
-              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-3 focus:ring-blue-500 focus:border-blue-500 h-32 resize-none"
-            ></textarea>
-          </div>
-
-          {error && <p className="text-red-500 mb-4">{error}</p>}
-          {success && <p className="text-green-500 mb-4">{success}</p>}
-
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-            >
-              Register
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-    </div>
-
   );
 };
 
